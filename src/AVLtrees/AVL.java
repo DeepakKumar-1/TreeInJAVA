a306d8b4fb0f1307b440f564efd93a6668c978c0
Deepak <dk8400779@gmail.com>
2023-08-10 22:02:24 +0530

    Initial Commit

--- src/AVLtrees/AVL.java
+++ src/AVLtrees/AVL.java
@@ -180,7 +180,7 @@ public class AVL {
 
     public static void main(String []args){
         AVL tree = new AVL();
-        for(int i=0; i<1000; i++){
+        for(int i=0; i<10; i++){
             tree.insert(i);
         }
         System.out.println(tree.height(tree.root));
